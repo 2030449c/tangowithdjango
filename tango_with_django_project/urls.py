abd931b3ce7469932c9ca5cbbc26c187fe5f4175
unknown <2030449c@BO706-23.ad.dcs.gla.ac.uk>
2015-01-26 12:42:41 +0000

    chapter 7 - completed

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
-    url(r'^gabe/', include('gabe.urls')),
-    url(r'^about/', include('about.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^rango/', include('rango.urls')),
 )
 
 if settings.DEBUG:
@@ -19,3 +14,7 @@ if settings.DEBUG:
         (r'^media/(?P<path>.*)',
         'serve',
         {'document_root': settings.MEDIA_ROOT}), )
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL,
+                              document_root=settings.STATIC_ROOT)
