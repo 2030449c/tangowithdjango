b0797e5695c5b54cfda9682a801490bca149d977
unknown <2030449c@BO706-09.ad.dcs.gla.ac.uk>
2015-02-27 19:04:33 +0000

    completed chapter 12

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
 
 urlpatterns = patterns('',
         url(r'^admin/', include(admin.site.urls)),
         url(r'^rango/', include('rango.urls')),
+        (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 if settings.DEBUG:
@@ -18,3 +20,16 @@ if settings.DEBUG:
 if not settings.DEBUG:
         urlpatterns += static(settings.STATIC_URL,
                               document_root=settings.STATIC_ROOT)
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
+
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
+        #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
+)
